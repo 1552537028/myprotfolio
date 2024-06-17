76ecd02a7a4e1c3be8292869da656b16e589a990
Jay <112299655+1552537028@users.noreply.github.com>
2024-06-17 15:09:16 +0530

    Update Grid.tsx

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -9,7 +9,7 @@ export function Grid() {
   <section id="Services">
     <p className='heading'>Services</p>
    <div className="w-full mt-12 grid grid-cols-1 lg:grid-cols-3 gap-7">
-   {items.map((item) =>(  <Button duration={Math.floor(Math.random()*10000)+1000} borderRadius ="1.75rem" className ="bg-white dark:bg-black-200 text-black dark:text-white border-neutral-200 dark:border-slate-900">
+   {items.map((item) =>(<Button duration={Math.floor(Math.random()*10000)+1000} borderRadius ="1.75rem" className ="bg-white dark:bg-black-200 text-black dark:text-white border-neutral-200 dark:border-slate-900">
      <div>
       {item.title}
      </div>
